15eef47316222c5b497be31e7118e4ffadd58fb3
Ties de Kock <ties@tiesdekock.nl>
2023-07-11 13:44:04 +0200

    Another attempt at github publication

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -22,8 +22,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/ties/maven-snapshot")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
