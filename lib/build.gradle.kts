461d95f4fadfc0a1da9a4909c81142611677b8e2
Ties de Kock <ties@tiesdekock.nl>
2023-07-11 13:33:49 +0200

    do not override name

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -13,7 +13,6 @@ plugins {
 }
 
 version = "0.1.0-SNAPSHOT"
-name = "rrdp-test-data"
 group = "com.github.ties"
 
 // https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-gradle-registry#example-using-kotlin-dsl-for-a-single-package-in-the-same-repository
